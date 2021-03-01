939854df1aee4578ee0400334daf3885aae7e8ea
Hamam-dev <misaanalyst2021@gmail.com>
2021-03-01 01:10:08 -0800

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'views/intro_screens/splash_screen.dart';
+import 'views/screens/appBottomBar.dart';
+import 'views/screens/choose_courses.dart';
+import 'views/screens/choose_plan.dart';
+import 'views/screens/course_enroll_screen.dart';
+import 'views/screens/login_screen.dart';
+import 'views/screens/signup_screen.dart';
 import 'views/widgets/loading_widget.dart';
 
 
@@ -14,7 +21,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Manahil',
     // theme: appTheme(),
-      home: Loading(),
+      home: Splash(),
+      routes: {
+        LoginScreen.routeName:(ctx)=>LoginScreen(),
+        SignUpScreen.routeName:(ctx)=>SignUpScreen(),
+        ChooseCourses.routeName:(ctx)=>ChooseCourses(),
+        ChoosePlan.routeName:(ctx)=>ChoosePlan(),
+        ManahilBottomBar.routeName:(ctx)=>ManahilBottomBar(),
+        CourseEnroll.routeName:(ctx)=>CourseEnroll(),
+      }, // Defining app routes
     );
   }
 }
+
+//how to use it :
+//Navigator.of(context).pushNamed(screenName.routeName);
